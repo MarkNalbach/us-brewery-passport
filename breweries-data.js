71263ff1a1a2a6bc166967f29f00217b40a46bf0
MarkNalbach <marknalbach@gmail.com>
2025-12-18 09:04:19 -0600

    Create breweries-data.js

---
+++ breweries-data.js
@@ -0,0 +1,14 @@
+// breweries-data.js
+const BREWERIES_BY_STATE = {
+  Idaho: [
+    { name: "Example Brewing Co", city: "Boise" },
+    { name: "Sample Taproom", city: "Coeur d'Alene" },
+    { name: "Demo Brewery", city: "Idaho Falls" },
+  ],
+  Colorado: [
+    // Coming soon
+  ],
+  California: [
+    // Coming soon
+  ],
+};
