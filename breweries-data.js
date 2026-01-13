f2eb48a154403d2289b5fa73955adb6cc7b00141
MarkNalbach <marknalbach@gmail.com>
2026-01-13 10:57:03 -0600

    Update breweries-data.js

--- breweries-data.js
+++ breweries-data.js
@@ -36,7 +36,7 @@ const breweriesByState = {
   "Ohio": [],
   "Oklahoma": ["Stonecloud Brewing"],
   "Oregon": [],
-  "Pennsylvania": ["Rural City Beer Company","Vault Brewing Co - Easton","Vault Brewing Co - Yardley","Vortex Brewing Company","Whiskers Brewing Inc"],
+  "Pennsylvania": ["Rural City Beer Company","ShawneeCraft Brewing Company","Vault Brewing Co - Easton","Vault Brewing Co - Yardley","Vortex Brewing Company","Whiskers Brewing Inc"],
   "Rhode Island": [],
   "South Carolina": [],
   "South Dakota": [],
