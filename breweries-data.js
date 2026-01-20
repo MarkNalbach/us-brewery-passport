da13068b8244f341a879e5ed9dc3ba4c4c6a5f65
MarkNalbach <marknalbach@gmail.com>
2026-01-20 10:14:08 -0600

    Update breweries-data.js

--- breweries-data.js
+++ breweries-data.js
@@ -20,7 +20,7 @@ const breweriesByState = {
   "Maine": [],
   "Maryland": [],
   "Massachusetts": ["Fieldcrest Brewing Company"],
-  "Michigan": ["HomeGrown Brew Co","Upper Peninsula Brewing Company"],
+  "Michigan": ["ConfluxCity Brewing Company","HomeGrown Brew Co","Upper Peninsula Brewing Company"],
   "Minnesota": ["August Schell Brewing Company"],
   "Mississippi": [],
   "Missouri": [],
