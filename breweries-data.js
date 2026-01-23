43d1e3693f04f97adf774912fd59c2d1d3a317fc
MarkNalbach <marknalbach@gmail.com>
2026-01-23 12:50:37 -0600

    Update breweries-data.js

--- breweries-data.js
+++ breweries-data.js
@@ -30,7 +30,7 @@ const breweriesByState = {
   "New Hampshire": ["Republic Brewing Company","White Mountain Brewing Company"],
   "New Jersey": ["Bent Iron Brewing Co.","Bullhide Brewery","Seven Sons Brewing","Toms River Brewing"],
   "New Mexico": ["Rowley Farmhouse Ales"],
-  "New York": ["Apex Brewery","Catskill Brewery","Bitters & Bones","Dancing Grain Farm Brewery","Gardiner Brewing Company","Simple Brewing","Singlecut Beersmiths","Six Harbors Brewimg Company","South Shore Craft Brewery","Take Two Brewery","The Ridge West Seneca","Twisted Rail Brewing Company - Canandaigua","Twisted Rail Brewing Company - Geneva","Twisted Rail Brewing Company - Honeoye","Twisted Rail Brewing Company - Manchester"],
+  "New York": ["Apex Brewery","Battle Hill Brewing Company","Catskill Brewery","Bitters & Bones","Dancing Grain Farm Brewery","Gardiner Brewing Company","Simple Brewing","Singlecut Beersmiths","Six Harbors Brewimg Company","South Shore Craft Brewery","Take Two Brewery","The Ridge West Seneca","Twisted Rail Brewing Company - Canandaigua","Twisted Rail Brewing Company - Geneva","Twisted Rail Brewing Company - Honeoye","Twisted Rail Brewing Company - Manchester"],
   "North Carolina": [],
   "North Dakota": ["Black Paws Brewing Company","Dialectic Brewing & Ciderhouse"],
   "Ohio": ["Combustion Brewery & Taproom - Clintonville","Combustion Brewery & Taproom - Pickerington","Hearth Brewing","Honest Friend Brewing","North High Brewing Dublin","North High Brewing Westerville","Twin Oast Brewing"],
