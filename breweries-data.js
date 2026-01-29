9b1be753f530223e5eb015254d69d23bdd00cd4d
MarkNalbach <marknalbach@gmail.com>
2026-01-29 12:36:23 -0600

    Update breweries-data.js

--- breweries-data.js
+++ breweries-data.js
@@ -23,7 +23,7 @@ const breweriesByState = {
   "Michigan": ["Austin Brothers Beer Co.","ConfluxCity Brewing Company","HomeGrown Brew Co","Lansing Brewing Company","Upper Peninsula Brewing Company"],
   "Minnesota": ["August Schell Brewing Company","Old No. 1 Brewhouse","Talking Waters Brewing Co."],
   "Mississippi": [],
-  "Missouri": ["Bluewood Brewing"],
+  "Missouri": ["Bluewood Brewing","Trail Stop Brewery"],
   "Montana": [],
   "Nebraska": ["Boss City Brewing Co."],
   "Nevada": ["Bad Beat Brewing"],
